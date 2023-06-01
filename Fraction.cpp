a7fb61089b6eeead44d18dfc119e08dbc58fe464
marcarro <A00837236@tec.mx>
2023-06-01 12:45:09 -0600

    Added stuff

--- Fraction.cpp
+++ Fraction.cpp
@@ -2,6 +2,8 @@
 #include <numeric>
 #include <iostream>
 
+// TODO: a, b, f, g
+
 Fraction::Fraction() {
   numerator = 0;
   denominator = 0;
@@ -13,8 +15,54 @@ Fraction::Fraction(int numer, int denom) {
   this->reduce();
 }
 
+// a) sum
+Fraction operator+(Fraction lhs, const Fraction& rhs) {
+  // do stuff
+}
+
+// b) subtract
+Fraction operator-(Fraction lhs, const Fraction& rhs) {
+  // do stuff
+}
+
+Fraction operator*(Fraction lhs, const Fraction& rhs) {
+  int newNum = lhs.numerator * rhs.numerator;
+  int newDen = lhs.denominator * rhs.denominator;
+  return Fraction(newNum, newDen);
+}
+
+Fraction operator/(Fraction lhs, const Fraction& rhs) {
+  int newNum = lhs.numerator * rhs.denominator;
+  int newDen = lhs.denominator * rhs.numerator;
+  return Fraction(newNum, newDen);
+}
+
+bool operator<(const Fraction& lhs, const Fraction& rhs) {
+  double lhsValue = lhs.numerator / (double)lhs.denominator;
+  double rhsValue = rhs.numerator / (double)rhs.denominator;
+  return lhsValue < rhsValue;
+}
+
+bool operator>(const Fraction& lhs, const Fraction& rhs) {
+  return rhs < lhs;
+}
+
+bool operator==(const Fraction& lhs, const Fraction& rhs) {
+  return ((lhs.numerator == rhs.numerator) && (lhs.denominator == rhs.denominator));
+}
+
 void Fraction::print() {
   std::cout << numerator << "/" << denominator << "\n";
+}
+
+// f) print w spaces
+void Fraction::print(int) {
+  // do stuff
+}
+
+// g) print in decimals
+void Fraction::printf() {
+  // do stuff
 }
 
 void Fraction::reduce() {
--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
+#include <iostream>
 #include "Fraction.h"
 using namespace std;
 
 int main() {
+  Fraction f1(1,2);
+  Fraction f2(4,2);
+
+  f1.print(), f2.print();
+
+  Fraction f3 = f1 / f2;
+  Fraction f4 = f1 * f2;
+
+  f3.print(), f4.print();
+
+  bool bigger = f1 > f2;
+  bool smaller = f1 < f2;
+  bool equal = f1 == f2;
+  cout << bigger << " " <<  smaller << " " << equal << "\n";
 }
